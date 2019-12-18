85ecea5d7e940040600ae31517a0c5248759e16a
jlott1999 <58454146+jlott1999@users.noreply.github.com>
2019-12-18 10:52:42 -0700

    Deployed

---
+++ First.js
@@ -0,0 +1,13 @@
+/*
+* Objects, Interfaces, and API's
+ */
+
+let dog = {
+    name: "Steve",
+    color: "brown",
+    breed: "pug",
+    size: "small",
+    bark: function (){
+        console.log("Bark!");
+    }
+};
